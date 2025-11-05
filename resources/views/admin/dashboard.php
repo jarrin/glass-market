08dbb2a0ce0519c78bfc4943b58ae42d3efac687
Pajer3 <musieatsbeha633@gmail.com>
2025-11-05 18:56:06 +0100

    refactor: Update dashboard links and styles for payment history and verification actions

--- resources/views/admin/dashboard.php
+++ resources/views/admin/dashboard.php
@@ -364,13 +364,13 @@ $admin_email = $_SESSION['user_email'] ?? '';
                     </div>
                     <span>Verify Customers</span>
                 </a>
-                <a href="payment-errors.php" class="action-btn" style="border: 2px solid #dc2626;">
-                    <div class="action-btn-icon" style="background: #dc2626;">
+                <a href="payment-history.php" class="action-btn" style="border: 2px solid #667eea;">
+                    <div class="action-btn-icon" style="background: #667eea;">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" width="24" height="24">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
                         </svg>
                     </div>
-                    <span style="color: #dc2626; font-weight: 700;">Payment Errors</span>
+                    <span>Payment History</span>
                 </a>
             </div>
         </div>
