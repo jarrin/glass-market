7dd8fcf2b3a2192f3cc8f1cffdd96b6a698f2268
Pajer3 <musieatsbeha633@gmail.com>
2025-11-05 18:37:14 +0100

    fix: Include subscription status check in sellers view to ensure users without access receive appropriate messaging

--- resources/views/sellers.php
+++ resources/views/sellers.php
@@ -1,5 +1,6 @@
 <?php session_start(); ?>
 <?php require_once __DIR__ . '/../../config.php'; ?>
+<?php require_once __DIR__ . '/../../includes/subscription-check.php'; ?>
 <!doctype html>
 <html lang="en">
 <head>
